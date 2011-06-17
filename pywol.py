6d7c88a1d9df820468d1fbc76a7cbf0d5a4fff39
Mike Grouchy <mgrouchy@gmail.com>
2011-06-17 12:47:12 -0700

    updated some formatting

--- pywol.py
+++ pywol.py
@@ -5,20 +5,26 @@ import re
 MAC_PATT = re.compile('([a-fA-F0-9]{2}[:|\-]?){6}')
 MAC_PARTS_PATT = re.compile('[a-fA-F0-9]{2}')
 
-def WOL(mac_addr, ip):
-    """comments go here"""
+def wake(mac_addr, ip):
+    """wake a computer given ip and mac address assuming wake
+       on lan is enabled
+    """
     
     #invalid ethernet address
-    if MAC_PATT.match(mac_addr) == None: return False
+    if MAC_PATT.match(mac_addr) == None: 
+        return False
 
     byte_addr = MAC_PARTS_PATT.findAll(mac_addr)
-    if len(byte_addr) != 6: return false
+    if len(byte_addr) != 6: 
+        return false
 
     mac_addrB = struct.pack('BBBBBB', *map(int, byte_addr,[16, 16, 16, 16, 16, 16]))
+    
     #construct the message for the wake on lan packet
     #WOL Magic Packet Format: 12 leading F's + target machines
     #MAC Address
     magic_packet = "F" * 12 + mac_addrB * 16
+    
     #create a datagram socket and send out packet
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
